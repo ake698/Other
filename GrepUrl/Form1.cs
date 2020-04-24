eca9e7dbaa3fc5cc7b825734eb7604f9f86555ae
shiqing.feng <shiqing.feng@starlight-sms.com>
2020-04-24 14:40:32 +0800

    添加链接过滤

---
+++ GrepUrl/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GrepUrl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        delegate void UpdateButtonDelegate(Button button,string text, bool status);
+        UpdateButtonDelegate updateButton;
+
+        //判断是否选择了文件
+        private bool HasFile1 = false;
+        private bool HasFile2 = false;
+        private bool FileDialog(TextBox textBox)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "所有文件(*txt*)|*.txt*"; //设置要选择的文件的类型
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;//返回文件的完整路径            
+                textBox.Text = file;
+                return true;
+                //Console.WriteLine(file);
+            }
+            return false;
+        }
+
+        public void UpdateButton(Button button,string text, bool status)
+        {
+            button.Enabled = status;
+            button.Text = text;
+        }
+        public void AsyncUpdate(string text, bool status)
+        {
+            this.Invoke(updateButton,this.button3, text, status);
+            this.Invoke(updateButton, this.button1, "选择", status);
+            this.Invoke(updateButton, this.button2, "选择", status);
+           
+        }
+
+        private void EnableButton3()
+        {
+            if (HasFile1 && HasFile2) this.button3.Enabled=true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HasFile1 =  this.FileDialog(this.textBox1);
+            this.EnableButton3();
+        }
+
+     
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HasFile2 = this.FileDialog(this.textBox2);
+            this.EnableButton3();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Work work = new Work(this.textBox1.Text,this.textBox2.Text);
+            work.updateButton = this.AsyncUpdate;
+            Thread thread = new Thread(new ThreadStart(work.Start));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.updateButton = UpdateButton;
+        }
+    }
+}
---
+++ GrepUrl/Work.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace GrepUrl
+{
+    class Work
+    {
+        public delegate void AsyncUpdateButton(string text, bool status);
+        public AsyncUpdateButton updateButton;
+        private string File1;
+        private string File2;
+        private List<string> Urls;
+        public Work(string file1,string file2)
+        {
+            this.File1 = file1;
+            this.File2 = file2;
+        }
+
+        public void Start()
+        {
+            updateButton("运行中", false);
+            Thread.Sleep(1000);
+            this.HandlerFile();
+            updateButton("选择文件", true);
+        }
+
+        private void GetUrls()
+        {
+            Urls = new List<string>();
+            FileStream file = new FileStream(this.File2, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Debug.WriteLine(line);
+                Urls.Add(line);
+            }
+            reader.Close();
+            file.Close();
+        }
+
+
+
+        public void HandlerFile()
+        {
+            this.GetUrls();
+            FileStream file = new FileStream(this.File1, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            string fileName = DateTime.Now.ToString("yyyy-MM-ddhhmmss");
+            FileStream writeFile = new FileStream(System.Environment.CurrentDirectory + "/" + "grep" + fileName + ".txt", FileMode.Create);
+            StreamWriter write = new StreamWriter(writeFile);
+            string line;
+            string[] result;
+            while((line = reader.ReadLine()) != null)
+            {
+                Debug.WriteLine(line);
+                result = this.HandlerLine(line);
+                if (result[0] == "none") continue;
+
+                if (ContainsUrl(result[0])) write.WriteLine(result[1]);
+            }
+
+            reader.Close();
+            file.Close();
+
+            write.Close();
+            writeFile.Close();
+        }
+
+        public bool ContainsUrl(string url)
+        {
+            foreach(string u in Urls)
+            {
+                if (u.Trim().Equals(url.Trim())) return true;
+            }
+            return false;
+        }
+
+        public string[] HandlerLine(string line)
+        {
+            string[] arrs;
+            string id;
+            string secren;
+            if (line.IndexOf("----") > -1)
+            {
+                Debug.WriteLine("第二种");
+                line = line.Replace("----", "*");
+                arrs = line.Split('*');
+                if (arrs.Length < 4) return new string[] { "none","" };
+                id = arrs[2];
+                secren = arrs[0] + "----" + arrs[1] + "----" + arrs[2];
+            }
+            else
+            {
+                Debug.WriteLine("第一种");
+                arrs = line.Split('|');
+                if (arrs.Length < 7) return new string[] { "none", "" };
+                id = arrs[5];
+                secren = "";
+            }
+            string url = "https://weibo.com/u/" + id;
+            return new string[] { url,secren};
+        }
+
+
+    }
+}
