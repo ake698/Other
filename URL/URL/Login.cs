94f7f0df7b678eda03e4524d12cfc369fc0771cb
hunk5431@gmail.com <hunk5431@gmail.com>
2020-06-18 19:06:54 +0800

    11

---
+++ URL/URL/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace URL
+{
+    class Login
+    {
+        public void SinaLogin(string url)
+        {
+            var request = Utils.GetCommonRequest(url);
+
+        }
+
+    }
+}
---
+++ URL/browerdemo/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace browerdemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string defaultUserAgent = null;
+        [DllImport("urlmon.dll", CharSet = CharSet.Ansi)]
+        private static extern int UrlMkSetSessionOption(int dwOption, string pBuffer, int dwBufferLength, int dwReserved);
+        const int URLMON_OPTION_USERAGENT = 0x10000001;
+        public static void ChangeUserAgent(string userAgent)
+        {
+            UrlMkSetSessionOption(URLMON_OPTION_USERAGENT, userAgent, userAgent.Length, 0);
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeUserAgent("Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36");
+            webBrowser1.Navigate("https://www.baidu.com/");
+        }
+
+    }
+}
