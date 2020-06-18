d537becef559abf4b169fdf6cd4163c9d2719c96
hunk5431@gmail.com <hunk5431@gmail.com>
2020-06-18 19:47:16 +0800

    添加账号过滤器

---
+++ GrepMobilePhone/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GrepMobilePhone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool HasFile = false;
+        delegate void UpdateButtonDelegate(Button button, string text, bool status);
+        UpdateButtonDelegate updateButton;
+
+        private bool FileDialog(TextBox textBox)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "所有文件(*txt*)|*.txt*"; //设置要选择的文件的类型
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;//返回文件的完整路径            
+                textBox.Text = file;
+                UpdateButton(button1,"开始", true);
+                return true;
+            }
+            return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!HasFile)
+            {
+                HasFile = this.FileDialog(this.textBox1);
+            }
+            else
+            {
+                
+                Work work = new Work(this.textBox1.Text);
+                work.updateButton = this.AsyncUpdate;
+                Thread thread = new Thread(new ThreadStart(work.Start));
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            
+            
+        }
+
+        public void UpdateButton(Button button, string text, bool status)
+        {
+            button.Enabled = status;
+            button.Text = text;
+        }
+        public void AsyncUpdate(string text, bool status)
+        {
+            this.Invoke(updateButton, this.button1, text, status);
+
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            this.updateButton = UpdateButton;
+        }
+    }
+}
---
+++ GrepMobilePhone/Work.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace GrepMobilePhone
+{
+
+    public class Work
+    {
+        public delegate void AsyncUpdateButton(string text, bool status);
+        public AsyncUpdateButton updateButton;
+        private string File1;
+        public Work(string file1)
+        {
+            this.File1 = file1;
+        }
+        public void Start()
+        {
+            updateButton("运行中", false);
+            Thread.Sleep(1000);
+            this.HandlerFile();
+            updateButton("开始", true);
+        }
+        public void HandlerFile()
+        {
+            FileStream file = new FileStream(this.File1, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            string fileName = DateTime.Now.ToString("yyyy-MM-ddhhmmss");
+            FileStream mailFile = new FileStream(System.Environment.CurrentDirectory + "/" + "mail" + fileName + ".txt", FileMode.Create);
+            FileStream phoneFile = new FileStream(System.Environment.CurrentDirectory + "/" + "phone" + fileName + ".txt", FileMode.Create);
+            StreamWriter mailWrite = new StreamWriter(mailFile);
+            StreamWriter phoneWrite = new StreamWriter(phoneFile);
+            string line;
+            bool result;
+            while ((line = reader.ReadLine()) != null)
+            {
+                result = this.IsMail(line);
+                if (result)
+                {
+                    mailWrite.WriteLine(line);
+                }
+                else
+                {
+                    phoneWrite.WriteLine(line);
+                }
+
+            }
+
+            reader.Close();
+            file.Close();
+
+            mailWrite.Close();
+            mailFile.Close();
+
+            phoneWrite.Close();
+            phoneFile.Close();
+        }
+
+
+        public bool IsMail(string line)
+        {
+            string[] arrs;
+            string username;
+            if (line.IndexOf("----") > -1)
+            {
+                //Debug.WriteLine("第1种");
+                line = line.Replace("----", "*");
+                arrs = line.Split('*');
+                if (arrs.Length < 3) return false;
+                username = arrs[0];
+                if (username.Contains("@")) return true;
+            }
+            return false;
+        }
+
+    }
+}
+
